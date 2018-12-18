083bf03e878d8dbcf0e5323de744f7b26e924a25
veno11 <45961084+veno11@users.noreply.github.com>
2018-12-18 03:24:00 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("524490444617285643")
+setInterval(function() {
+channel.send(` veno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backkveno veno veno veno veno veno is backk veno veno veno veno veno veno is backk`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
